86eda10b591181244e689f62aa7e147428be44d8
Alexandre <167219969+alexandreJJP@users.noreply.github.com>
2024-04-17 15:34:30 -0300

    Add files via upload

--- Fokus (javascript)/script.js
+++ Fokus (javascript)/script.js
@@ -5,25 +5,37 @@ function log(texto){
 }
 
 // VARIÁVEIS BOTÕES
-const mainButton = document.getElementById('start-pause')
+const mainButton = document.querySelector('#start-pause')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const botoes = document.querySelectorAll('.app__card-button')
-const musicaInput = document.querySelector('#alternar-musica')
+const botaoMusica = document.querySelector('#alternar-musica')
 
-// VARIÁVEIS ELEMENTOS GRÁFICOS
+// VARIÁVEIS ELEMENTOS
 const displayTemporizador = document.querySelector('.app__card-timer')
 const imagemPrincipal = document.querySelector('.app__image')
 const tituloPrincipal = document.querySelector('.app__title')
+const imagemBotao = document.querySelector('.app__card-primary-butto-icon')
+const tempoNaTela = document.querySelector('#timer')
+
+// VÁRIAVEIS SONS
+const musica = new Audio("/sons/luna-rise-part-one.mp3")
+musica.loop = true
+
+const AudioTempoFinalizado = new Audio("/sons/beep.mp3")
+AudioTempoFinalizado.volume = 0.3;
+
+const AudioPlay = new Audio("/sons/play.wav")
+
+const AudioPause = new Audio("/sons/pause.mp3")
+
 
 // VARIÁVEIS DE TEMPO
-const tempoFoco = 1500
-const tempoDescanso = 300
-const tempoLongo = 900
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
 
 // FUNÇÕES
-
 function alterarContexto(contexto){
     // retira classe do botao
     botoes.forEach(function (contexto){
@@ -53,7 +65,65 @@ function alterarContexto(contexto){
     }
 }
 
+function changeMainButton(texto, icone){
+    mainButton.innerHTML = `<img class="app__card-primary-butto-icon" src="/imagens/${icone}.png" alt="">
+    <span>${texto}</span>`
+}
+
+function contagemRegressiva(){
+    if (tempoDecorridoEmSegundos == 0){
+        //AudioTempoFinalizado.play()
+        musica.pause()
+        pausar()
+
+        alert('Tempo finalizado!')
+        tempoDecorridoEmSegundos = 5
+        changeMainButton('Começar', 'play_arrow')
+        
+        return
+    }
+
+    tempoDecorridoEmSegundos -= 1
+    mostrarTempo()
+}
+
+function iniciar(){
+    if (intervaloId){
+        pausar()
+        return
+    }
+    changeMainButton('Pausar', 'pause')
+    AudioPlay.play()
+    intervaloId = setInterval(contagemRegressiva, 1000)
+    
+}
+
+function pausar(){
+    changeMainButton('Continuar', 'play_arrow')
+    AudioPause.play()
+    clearInterval(intervaloId)
+    intervaloId = null
+}
+
+function mostrarTempo(){
+    const tempo = tempoDecorridoEmSegundos
+    tempoNaTela.innerHTML = `${tempo}`
+
+}
+
+mostrarTempo()
+
 // EVENTOS
+
+mainButton.addEventListener('click', iniciar)
+
+botaoMusica.addEventListener('change', () => {
+    if(musica.paused){
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
 
 focoBt.addEventListener('click', () =>{
     alterarContexto('foco')
